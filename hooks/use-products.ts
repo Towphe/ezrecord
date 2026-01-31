f02513b8455db128866cb2f2892450b0c55480fa
Towphe <castillochristopherjames@earlyref.com>
2026-01-31 11:55:29 +0800

    chore: memoized products fetching

--- hooks/use-products.ts
+++ hooks/use-products.ts
@@ -2,36 +2,39 @@ import { Product } from "@/types/products.ts";
 import { desc, like } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useSQLiteContext } from "expo-sqlite";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import * as schema from "../db/schema.ts";
 
 export function useProducts() {
   const db = useSQLiteContext();
-  const drizzleDb = drizzle(db, { schema });
+  const drizzleDb = useMemo(() => drizzle(db, { schema }), [db]);
 
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchProducts = async (name?: string) => {
-    try {
-      const baseQuery = drizzleDb.select().from(schema.product);
+  const fetchProducts = useCallback(
+    async (name?: string) => {
+      try {
+        const baseQuery = drizzleDb.select().from(schema.product);
 
-      const data =
-        name && name !== ""
-          ? await baseQuery.where(like(schema.product.name, `%${name}%`))
-          : await baseQuery.orderBy(desc(schema.product.createdAt)).limit(10);
-      setProducts(data);
-    } catch (err) {
-      console.log("Error fetching products:", err);
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  };
+        const data =
+          name && name !== ""
+            ? await baseQuery.where(like(schema.product.name, `%${name}%`))
+            : await baseQuery.orderBy(desc(schema.product.createdAt)).limit(10);
+        setProducts(data);
+      } catch (err) {
+        console.log("Error fetching products:", err);
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [drizzleDb],
+  );
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [fetchProducts]);
 
   return { products, loading, refetch: fetchProducts };
 }
