e3f505bcc8151269820da3df5a3cd06bdbc9a740
Towphe <castillochristopherjames@earlyref.com>
2026-01-31 12:22:49 +0800

    feat: created hook for deleting products

---
+++ hooks/delete-product.ts
@@ -0,0 +1,41 @@
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { useSQLiteContext } from "expo-sqlite";
+import * as schema from "../db/schema.ts";
+
+export function useDeleteProduct(productId: string) {
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db, { schema });
+
+  const deleteProduct = async () => {
+    try {
+      const product = await drizzleDb
+        .select()
+        .from(schema.product)
+        .where(eq(schema.product.productId, productId))
+        .get();
+
+      if (!product) {
+        throw new Error("Product not found");
+      }
+
+      const updatedProduct = {
+        isDeleted: 1,
+      };
+
+      const result = await drizzleDb
+        .update(schema.product)
+        .set(updatedProduct)
+        .where(eq(schema.product.productId, productId))
+        .returning();
+
+      console.log(result);
+
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return { deleteProduct };
+}
