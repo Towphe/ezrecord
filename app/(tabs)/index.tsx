ea3dcfbfbf15376967106dfb5058c060a5e8e2f2
Towphe <castillochristopherjames@earlyref.com>
2026-01-31 11:55:11 +0800

    fix: added random-values package

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { StyleSheet } from "react-native";
+import "react-native-get-random-values";
 
 export default function StatisticsScreen() {
   return (
