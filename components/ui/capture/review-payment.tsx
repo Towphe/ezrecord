f11b69b5ad912814876201e603bd0eea25f0e38d
Towphe <castillochristopherjames@earlyref.com>
2026-02-07 15:13:13 +0800

    chore: styled form

--- components/ui/capture/review-payment.tsx
+++ components/ui/capture/review-payment.tsx
@@ -2,6 +2,7 @@ import { CaptureStackParamList } from "@/app/(tabs)/capture";
 import { InputField } from "@/components/input-field";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
 import { useCreateTransaction } from "@/hooks/create-transaction";
 import zodResolver from "@/utils/zodResolver";
 import { RouteProp, useNavigation } from "@react-navigation/native";
@@ -93,34 +94,44 @@ export default function ReviewPayment({
 
   return (
     <ParallaxScrollView title="Products">
-      <KeyboardAvoidingView style={styles.form} behavior="height">
-        <InputField
-          fieldName="amount"
-          label="Amount"
-          control={control}
-          defaultValue={paymentDetails.amount}
-          placeholder="Amount"
-          disabled={!isEditing}
-        />
-        {errors.amount && <ThemedText>{errors.amount.message}</ThemedText>}
-        <InputField
-          fieldName="referenceNumber"
-          label="Reference Number"
-          control={control}
-          placeholder="Reference Number"
-          defaultValue={paymentDetails.referenceNumber}
-          disabled={!isEditing}
-        />
-        {errors.referenceNumber && (
-          <ThemedText>{errors.referenceNumber.message}</ThemedText>
-        )}
-        <Button
-          title="Confirm Payment"
-          color="green"
-          onPress={handleSubmit(confirmPayment)}
-        />
-        <Button title="Rescan" color="teal" onPress={() => redirectToScan()} />
-      </KeyboardAvoidingView>
+      <ThemedView style={{ flex: 1, justifyContent: "space-between" }}>
+        <KeyboardAvoidingView style={styles.form} behavior="height">
+          <InputField
+            fieldName="amount"
+            label="Amount"
+            control={control}
+            defaultValue={paymentDetails.amount}
+            placeholder="Amount"
+            disabled={!isEditing}
+            inputStyle={styles.input}
+          />
+          {errors.amount && <ThemedText>{errors.amount.message}</ThemedText>}
+          <InputField
+            fieldName="referenceNumber"
+            label="Reference Number"
+            control={control}
+            placeholder="Reference Number"
+            defaultValue={paymentDetails.referenceNumber}
+            disabled={!isEditing}
+            inputStyle={styles.input}
+          />
+          {errors.referenceNumber && (
+            <ThemedText>{errors.referenceNumber.message}</ThemedText>
+          )}
+        </KeyboardAvoidingView>
+        <ThemedView style={styles.actionButtons}>
+          <Button
+            title="Confirm Payment"
+            color="green"
+            onPress={handleSubmit(confirmPayment)}
+          />
+          <Button
+            title="Rescan"
+            color="teal"
+            onPress={() => redirectToScan()}
+          />
+        </ThemedView>
+      </ThemedView>
     </ParallaxScrollView>
   );
 }
@@ -132,6 +143,20 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     gap: 15,
+    width: "95%",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 12,
+    borderRadius: 8,
+    marginTop: 2,
+    fontSize: 16,
+    paddingHorizontal: 12,
+  },
+  actionButtons: {
+    gap: 6,
+    marginHorizontal: "auto",
     width: "95%",
   },
 });
